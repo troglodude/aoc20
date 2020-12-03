421cd401bb4871ebe92a936a17d3ba9406230e89
troglodude <31863107+troglodude@users.noreply.github.com>
2020-12-03 21:07:44 +0100

    Clean up solution for day 3

--- day03.lisp
+++ day03.lisp
@@ -2,74 +2,27 @@
 
 (defun read-input (in-path)
   (with-open-file (s in-path)
-    (let ((w 0)
-          (h 0))
-      (values
-       (loop for line = (read-line s nil) while line
-            do (incf h)
-               (setf w (length line))
-             collect line)
-       w h))))
+    (loop for line = (read-line s nil) while line
+            collect line)))
 
-(defun count-trees-3-1 (lol)
+(defun trees-with-slope (&key right down list)
   (let ((xpos 0)
-        (ll (length (car lol)))
+        (len (length (car list)))
         (num-trees 0))
-    (loop for l in lol do
-      (when (char= #\# (char l xpos))
-        (incf num-trees))
-      (setf xpos (mod (+ xpos 3) ll)))
-    num-trees))
+     (loop for l in list by (if right right #'cdr) do
+       (when (char= #\# (char l xpos))
+         (incf num-trees))
+       (setf xpos (mod (+ xpos (if down down 1)) len)))
+     num-trees))
 
 (defun sol1()
   (-<> (read-input "./input/day03")
-       (count-trees-3-1 <>)))
-
-(defun count-trees-1-1 (lol)
-  (let ((xpos 0)
-        (ll (length (car lol)))
-        (num-trees 0))
-    (loop for l in lol do
-      (when (char= #\# (char l xpos))
-        (incf num-trees))
-      (setf xpos (mod (+ xpos 1) ll)))
-    num-trees))
-
-(defun count-trees-5-1 (lol)
-  (let ((xpos 0)
-        (ll (length (car lol)))
-        (num-trees 0))
-    (loop for l in lol do
-      (when (char= #\# (char l xpos))
-        (incf num-trees))
-      (setf xpos (mod (+ xpos 5) ll)))
-    num-trees))
-
-(defun count-trees-7-1 (lol)
-  (let ((xpos 0)
-        (ll (length (car lol)))
-        (num-trees 0))
-    (loop for l in lol do
-      (when (char= #\# (char l xpos))
-        (incf num-trees))
-      (setf xpos (mod (+ xpos 7) ll)))
-    num-trees))
-
-(defun count-trees-1-2 (lol)
-  (let ((xpos 0)
-        (ll (length (car lol)))
-        (num-trees 0))
-    (loop for l in lol by #'cddr do
-      (when (char= #\# (char l xpos))
-        (incf num-trees))
-      (setf xpos (mod (+ xpos 1) ll)))
-    num-trees))
+       (trees-with-slope :down 3 :list <>)))
 
 (defun sol2 ()
-  (let* ((inp (read-input "./input/day03"))
-         (1-1 (count-trees-1-1 inp))
-         (3-1 (count-trees-3-1 inp))
-         (5-1 (count-trees-5-1 inp))
-         (7-1 (count-trees-7-1 inp))
-         (1-2 (count-trees-1-2 inp)))
-    (* 1-1 3-1 5-1 7-1 1-2)))
+  (let ((inp (read-input "./input/day03")))
+    (* (trees-with-slope :down 3 :list inp)
+       (trees-with-slope :down 1 :list inp)
+       (trees-with-slope :down 5 :list inp)
+       (trees-with-slope :down 7 :list inp)
+       (trees-with-slope :right #'cddr :list inp))))
