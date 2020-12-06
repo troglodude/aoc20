08dfaa3deba37dd218f4e0de26ac29219c986027
troglodude <31863107+troglodude@users.noreply.github.com>
2020-12-06 17:25:17 +0100

    Rename some variables to make cod more understandable
    Also, fix missing `loop' in `sol1' (solution was found in REPL and
    later written up, omitting `loop')

--- day04.lisp
+++ day04.lisp
@@ -1,6 +1,6 @@
 (in-package :day04)
 
-(defun read-input (inpath)
+(defun read-paragraphs (inpath)
   (with-open-file (s inpath)
     (labels ((sjoin (s)
                (format nil "~{~a~^ ~}" s)))
@@ -16,44 +16,44 @@
                  (push (sjoin pp) pps))
         pps))))
 
-(defparameter *mandatory-fields* '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid"))
-
-(defun valid-1-p (pp)
-  (loop for m in *mandatory-fields* do
-    (unless (search m pp)
-      (unless (string= m "cid")
+(defun valid-1-p (passport-data)
+  (loop for mandatory-key in '("byr" "iyr" "eyr" "hgt" "hcl" "ecl" "pid" "cid") do
+    (unless (search mandatory-key passport-data)
+      (unless (string= mandatory-key "cid")
         (return-from valid-1-p nil))))
   t)
 
 (defun sol1 ()
-  (-<> (read-input "./input/day04")
-       (valid-1-p <>)))
+  (-<> (read-paragraphs "./input/day04")
+       (loop for passport-data in <>
+             if (valid-1-p passport-data) count 1)))
 
-(defun valid-2-p (pp)
-  (and (multiple-value-bind (m kv)
-           (ppcre:scan-to-strings "(byr):([0-9]{4})\\b" pp)
-         (when m
-           (<= 1920 (parse-integer (aref kv 1)) 2002)))
-       (multiple-value-bind (m kv)
-           (ppcre:scan-to-strings "(iyr):([0-9]{4})\\b" pp)
-         (when m
-           (<= 2010 (parse-integer (aref kv 1)) 2020)))
-       (multiple-value-bind (m kv)
-           (ppcre:scan-to-strings "(eyr):([0-9]{4})\\b" pp)
-         (when m
-           (<= 2020 (parse-integer (aref kv 1)) 2030)))
-       (ppcre:scan-to-strings "(hcl):(#[0-9a-f]{6})\\b" pp)
-       (ppcre:scan-to-strings "(ecl):(amb|blu|brn|gry|grn|hzl|oth)\\b" pp)
-       (ppcre:scan-to-strings "(pid):([0-9]{9})\\b" pp)
-       (multiple-value-bind (m kv)
-           (ppcre:scan-to-strings "(hgt):([0-9]+)(cm|in)\\b" pp)
-         (when m
-           (let ((dim (aref kv 2))
-                 (val (parse-integer (aref kv 1))))
+(defun valid-2-p (passport-data)
+  (and (multiple-value-bind (match key-val)
+           (ppcre:scan-to-strings "byr:([0-9]{4})\\b" passport-data)
+         (when match
+           (<= 1920 (parse-integer (aref key-val 0)) 2002)))
+       (multiple-value-bind (match key-val)
+           (ppcre:scan-to-strings "iyr:([0-9]{4})\\b" passport-data)
+         (when match
+           (<= 2010 (parse-integer (aref key-val 0)) 2020)))
+       (multiple-value-bind (match key-val)
+           (ppcre:scan-to-strings "eyr:([0-9]{4})\\b" passport-data)
+         (when match
+           (<= 2020 (parse-integer (aref key-val 0)) 2030)))
+       (ppcre:scan "hcl:(#[0-9a-f]{6})\\b" passport-data)
+       (ppcre:scan "ecl:(amb|blu|brn|gry|grn|hzl|oth)\\b" passport-data)
+       (ppcre:scan "pid:([0-9]{9})\\b" passport-data)
+       (multiple-value-bind (match key-val)
+           (ppcre:scan-to-strings "hgt:([0-9]+)(cm|in)\\b" passport-data)
+         (when match
+           (let ((dim (aref key-val 1))
+                 (val (parse-integer (aref key-val 0))))
              (if (string= dim "cm")
                  (<= 150 val 193)
                  (<= 59 val 76)))))))
 
 (defun sol2 ()
-  (-<> (read-input "./input/day04")
-       (loop for pp in <> if (valid-2-p pp) count 1)))
+  (-<> (read-paragraphs "./input/day04")
+       (loop for passport-data in <>
+             if (valid-2-p passport-data) count 1)))
